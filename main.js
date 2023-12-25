223b81cb972bd62c37009055e93953f58d939302
Metaork2 <duncan-m-smith@hotmail.co.uk>
2023-12-25 23:38:38 +0000

    Swapped to using the PlaylistId API and using each channel's uploads playlist instaed of the search API

--- main.js
+++ main.js
@@ -10,35 +10,27 @@ const { discord,settings } = require('./config.json');
 var CurrentUsage = 0;
 var LatestVideoId = [];
 var channelIcon = [];
+var uploadsPlaylist = [];
 var state = "starting";
 
 
 //Initialization - Grab channel icons of configured channels    
 async function initialize(){
-
     for (i=0;i<settings.YoutubeChannelId.length;i++){
-
-
-        response = await fetch(`https://youtube.googleapis.com/youtube/v3/channels?part=snippet&id=${settings.YoutubeChannelId[i]}&key=${settings.YoutubeAPI}`)
-
+        response = await fetch(`https://youtube.googleapis.com/youtube/v3/channels?part=snippet&part=contentDetails&id=${settings.YoutubeChannelId[i]}&key=${settings.YoutubeAPIKey}`)
         if (response.ok){
-            json = await response.json();   
-
+            json = await response.json().then( json=>{
                 channelIcon[settings.YoutubeChannelId[i]] = { "large": json.items[0].snippet.thumbnails.medium, "default": json.items[0].snippet.thumbnails.default };
-
-
-            state = "ready";
-        }    
-
-
-
+                uploadsPlaylist[settings.YoutubeChannelId[i]] = json.items[0].contentDetails.relatedPlaylists.uploads;
+                state = "ready";
+            });
+        }  else{
+            console.log(`Initialization Error: ${response.status} - ${response.statusText}`);
+        }  
     }
 
     console.log(`Please ensure bot has permission to access and message in the configured discord server channel.\n Use the following link to "invite" the bot to your server https://discord.com/oauth2/authorize?client_id=${discord.clientid}&permissions=${discord.scopes}&scope=bot`)
-    
-
 }
-
 
 /*
 createDisordPing(array)
@@ -47,16 +39,17 @@ inputs: Array of Video Objectss
 Outputs:NULL
 */
 async function createDiscordPing(videos){
-
     console.log(videos);
-
     for (i=0;i<videos.length;i++){
-
         videoObject = videos[i];
-
         console.log(videoObject);
-        
         //ToDo: Per channel filtering (i.e shorts only or not, ignore vidoes without tags)
+
+        if (settings.channelContentFilter[videoObject.channelId] !== "all"){
+            if (settings.channelContentFilter[videoObject.channelId] !== videoObject.contentType){
+                return;
+            }
+        }
 
 
         //Bot requires no rich presence, ability to read or respond to messages - therefore discord's basic HTTP api can be used to simplify app
@@ -91,8 +84,7 @@ async function createDiscordPing(videos){
                 "url": `https://youtube.com/watch?v=${videoObject.videoId}`
                 }
             ]
-            
-        var $textMessage = "@everyone Check out the latest youtube video"
+        var $textMessage = `@everyone Check out the latest YouTube video from ${videoObject.channelName}`
         
         //POST {$discord.api.base}/channels/{$discord.channelid}/messages
         var $req = (`${discord.api.base}/channels/${discord.channelid}/messages`);
@@ -108,18 +100,13 @@ async function createDiscordPing(videos){
                 }
             })
         }
-
         response = await fetch($req,$opts)
-
         if (response){
             json = await response.json();
             console.log("Discord Ping Successful");
         }
-
         if (response.errors){
-
             console.log(response.errors);
-
         }
     }
 }
@@ -148,17 +135,13 @@ function interp(a){
     }
 
     createDiscordPing(output);
-    console.log("Videos in buffer:");
-  //  console.log(output);
     console.log("Latest Video ID's: "+LatestVideoId)
     state = "ready"
 }
 
-
 /*
 grabVideoData(array)
 Interrogates the Youtube API to retreive detailed information relating to the video IDs supplied
-
 inputs: Array of Youtube Video IDs
 Outputs:Array of Youtube Video Objects
 */
@@ -166,7 +149,7 @@ async function grabVideoData(a){
     if (a.length < 1){return;}else{
     queryString = '';
     a.forEach(e=>{queryString += "&id="+e});
-        response = await fetch('https://youtube.googleapis.com/youtube/v3/videos?part=snippet&part=contentDetails'+queryString+'&key='+settings.YoutubeAPI,{'method': "GET"});
+        response = await fetch('https://youtube.googleapis.com/youtube/v3/videos?part=snippet&part=contentDetails'+queryString+'&key='+settings.YoutubeAPIKey,{'method': "GET"});
         if (response.ok){
             json = await response.json();   
             return Promise.resolve(json.items);
@@ -181,52 +164,46 @@ inputs: Interger position of channel ID within constant array
 Outputs: array of Youtube video IDs
 */
 async function grabLatestVideos(selector){
-    response = await fetch('https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId='+settings.YoutubeChannelId[selector]+'&order=date&key='+settings.YoutubeAPI,{'method': "GET"});
+    console.log('https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&playlistId='+uploadsPlaylist[settings.YoutubeChannelId[selector]]+'&key='+settings.YoutubeAPIKey);
+    response = await fetch('https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&playlistId='+uploadsPlaylist[settings.YoutubeChannelId[selector]]+'&key='+settings.YoutubeAPIKey,{'method': "GET"});
     if (response.ok){
         json = await response.json();
         validVideos = [];
         for (i=0;i<json.items.length;i++){
-            if(LatestVideoId[selector] == json.items[i].id.videoId){
+            if(LatestVideoId[selector] == json.items[i].contentDetails.videoId){
                 i=6;
             }else{
-              validVideos.push(json.items[i].id.videoId);
+              validVideos.push(json.items[i].contentDetails.videoId);
             }
         }
         if (validVideos.length >=1){
             LatestVideoId[selector] = validVideos[0];
            videoData =  await grabVideoData(validVideos);
            await interp(videoData);
+           console.log(`Scheduling next detection run for ${new Date(Date.now()+900000).toTimeString()}`);
         }else{
+            console.log(`no new videos found, scheduling next detection run for ${new Date(Date.now()+900000).toTimeString()}`);
             state = "ready"
         }
+    }else{
+
+        console.log(`An Error has occurred, scheduling next detection run for ${new Date(Date.now()+900000).toTimeString()}`)
+        json = await response.json();
+        console.log(json.error.message);
     }
 }  
 
-
-
-
-
-//Repeatedly (5 minutes intervals? depending on youtube quota) check for new uploads, then determine content type
+//Repeatedly (15 minutes intervals? depending on youtube quota) check for new uploads, then determine content type
 function loop(){
-
     console.log("Checking for new videos");
-
-
     if (state == "updating" || state == "starting"){return false;} //Dont attempt to refresh if we're alreaday trying to
-
     var state ="updating"
-
     videoData = [];
-
     for (i=0;i<settings.YoutubeChannelId.length;i++){
-
         grabLatestVideos(i);
-
     }
-
-
 }
 
 initialize();
-//loop();
-setInterval(loop,3000);
+setTimeout(loop,5000); // Run first detection after 5 seconds
+setInterval(loop,900000);
